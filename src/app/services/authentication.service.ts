cbe492e8865f4e0d3aca48b469adcfdbaecefacf
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-05-14 08:59:19 -0300

    Criando serviço de login e logout

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../@core/data/users';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(
+            JSON.parse(localStorage.get('currentUser'))
+        );
+
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string): Observable<any> {
+        return this.http.post(`${environment.api}`, {
+            username,
+            password
+        }).pipe(map(this.handleLogin))
+    }
+
+    logout(): void {
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+
+    private handleLogin(user) {
+        if (user && user.token) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+        }
+
+        return user;
+    }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,4 +10,5 @@
 
 export const environment = {
   production: false,
+  api: 'http://localhost:3333'
 };
