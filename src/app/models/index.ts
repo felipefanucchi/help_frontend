6fc02e33dae0d0f13de7008d7043b2f7d499691d
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-05-14 08:27:52 -0300

    criando enums para identificar as roles durante o projeto todo

---
+++ src/app/models/index.ts
@@ -0,0 +1,5 @@
+import { Role } from './roles';
+
+export default {
+    Role
+};
---
+++ src/app/models/roles.ts
@@ -0,0 +1,6 @@
+export enum Role {
+    Admin = 'Admin',
+    Help = 'Help',
+    Professional = 'Professional',
+    Customer = 'Customer'
+}
