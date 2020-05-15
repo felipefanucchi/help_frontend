e9b0ce3384aac98701003d872f6b4366cf057014
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-05-15 08:15:15 -0300

    Criando interceptor para erros especificos de permissao e retorna erros comuns, inclusivo os de permissao

---
+++ src/app/helpers/error.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpHandler, HttpEvent, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from '../services';
+import { Observable, OperatorFunction, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    fordiddenStatuscode = [401, 403];
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private router: Router
+    ) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(this.handleErrors));
+    }
+
+    private handleErrors(err: HttpErrorResponse): Observable<HttpEvent<any>> {
+        if (this.fordiddenStatuscode.indexOf(err.status) !== -1) {
+            this.authenticationService.logout();
+            this.router.navigateByUrl('/', {
+                skipLocationChange: true
+            })
+        }
+
+        const error = err.error.message || err.statusText;
+
+        return throwError(error);
+    }
+}
