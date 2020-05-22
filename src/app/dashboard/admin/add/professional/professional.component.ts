0c66836914ed2983189525ca738ee6d265b47e92
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-21 23:27:12 -0300

    Remove sinal de maior excedente no template do componente de profissionais

--- src/app/dashboard/admin/add/professional/professional.component.ts
+++ src/app/dashboard/admin/add/professional/professional.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: 'admin-add-professional',
-	template: '<app-create-user [custom_fields]="fields"></app-create-user>>'
+	template: '<app-create-user [custom_fields]="fields"></app-create-user>'
 })
 export class AddProfessionalComponent {
 	fields = [];
