fd79f9d5d82be0fa70c0798ff17654adcd230f28
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-23 15:29:54 -0300

    Remove campos adicionais do formulario de membros da equipe help e admin

--- src/app/dashboard/admin/add/administrator/administrator.component.ts
+++ src/app/dashboard/admin/add/administrator/administrator.component.ts
@@ -4,45 +4,12 @@ import { Component } from "@angular/core";
 	selector: 'admin-add-administrator',
 	template: `
 	<app-create-user 
-		[custom_fields]="fields" 
+		[custom_fields]="[]" 
 		role="Administrador" 
 		(event_submitted)="handleSubmit($event)"
 	></app-create-user>`
 })
 export class AddAdministratorComponent {
-	fields = [
-		{
-			type: 'text',
-			label: 'Preço',
-			placeholder: 'R$ 99,90',
-			name: 'cost'
-		},
-		{
-			type: 'text',
-			label: 'Número de Registro',
-			placeholder: ''
-		},
-		{
-			type: 'radio',
-			label: 'Serviços',
-			name: 'service',
-			options: [
-				{
-					value: 'speech_therapist',
-					label: 'Fonoaudiólogo'
-				},
-				{
-					value: 'phisiotherapy',
-					label: 'Fisioterapeuta'
-				},
-				{
-					value: 'occupational_therapy',
-					label: 'Terapia Ocupacional'
-				},
-			]
-		}
-	];
-	
 	constructor() {}
 
 	handleSubmit(data) {
